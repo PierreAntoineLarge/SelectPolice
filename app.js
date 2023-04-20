0049a87b93eeb3afd6e4eff48c9df88dea4fecaa
pa large <pierreantoine.large@gmail.com>
2023-04-20 17:30:28 +0200

    modification

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 // JavaScript
-document.getElementById('monMenuDeroulant').addEventListener('change', function(event) {
-    var policeSelectionnee = event.target.value;
-    var texte = document.getElementById('monTexte');
-    switch (policeSelectionnee) {
+document.getElementById('monMenuDeroulant1').addEventListener('change', function(event) {
+    var policeSelectionnee1 = event.target.value;
+    var texte1 = document.getElementById('monTexte1');
+    switch (policeSelectionnee1) {
       case 'police1':
-        texte.style.fontFamily = 'Arial';
+        texte1.style.fontFamily = 'Arial';
         break;
       case 'police2':
-        texte.style.fontFamily = 'Times New Roman';
+        texte1.style.fontFamily = 'Times New Roman';
         break;
       case 'police3':
-        texte.style.fontFamily = 'Verdana';
+        texte1.style.fontFamily = 'Verdana';
         break;
       default:
-        texte.style.fontFamily = '';
+        texte1.style.fontFamily = '';
+    }
+  });
+
+  document.getElementById('monMenuDeroulant2').addEventListener('change', function(event) {
+    var policeSelectionnee2 = event.target.value;
+    var texte2 = document.getElementById('monTexte2');
+    switch (policeSelectionnee2) {
+      case 'police1':
+        texte2.style.fontFamily = 'Arial';
+        break;
+      case 'police2':
+        texte2.style.fontFamily = 'Times New Roman';
+        break;
+      case 'police3':
+        texte2.style.fontFamily = 'Verdana';
+        break;
+      default:
+        texte2.style.fontFamily = '';
     }
   });
   
+  document.getElementById('montexte')
+
